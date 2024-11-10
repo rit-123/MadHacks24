c18d37b0f400c3a15b9ecfba5e23b75e3fd09e3a
rit-123 <diamondfox2264@gmail.com>
2024-11-09 19:09:49 -0600

    login frontend done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,13 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import MainPage from './MainPage'
+import LoginPage from './Login'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-        <MainPage></MainPage>
+        <LoginPage></LoginPage>
     </>
   )
 }
